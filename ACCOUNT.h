478556450b4f0e9dd5811a8387a865e601863abe
sotirisfrost <102031427+sotirisfrost@users.noreply.github.com>
2022-03-21 14:09:51 +0200

    Added Virtual ATM

---
+++ ACCOUNT.h
@@ -0,0 +1,40 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+class ACCOUNT
+{
+	public:
+		//default ClientData constructor
+		ACCOUNT(int = 0, string = "", double = 0.00, double = 0.00);
+		// accessor functions for accountNumber
+		void setAccountNumber( int );
+		int getAccountNumber() const;
+		// accessor functions for Name
+		void setName( string );
+		string getName() const;
+		// accessor functions for balance
+		void setBalance( double );
+		double getBalance() const;
+		// accessor functions for limit
+		void setLimit( double );
+		double getLimit() const;
+		// function to check if the file exists
+		bool is_file_exist(const char *);
+		// Functions
+		void enterClient();
+		void check(int);
+		void moneyTransfer(int, double, double);
+		void newClient(int, double);
+		void outputLine(ostream&, const ACCOUNT &);
+		void printRecords(const char *);
+	private:
+		int accountNumber;
+		char name[ 25 ];
+		double balance;
+		double limit;
+};
+#endif
---
+++ ACCOUNT_MAIN.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cstring>
+#include "ACCOUNT.h"
+
+using namespace std;
+
+int main()
+{
+	ACCOUNT client;
+	bool checkFile = client.is_file_exist("ACCT.dat");
+	
+	if (checkFile == false)
+	{
+		ofstream outCreditSystem("ACCT.dat", ios::binary);
+		ACCOUNT blankClient;
+		for (int i = 0; i < 200; i++)
+		outCreditSystem.write(reinterpret_cast<const char*>(&blankClient),sizeof(ACCOUNT));
+	}
+	ifstream inCreditSystem("ACCT.dat", ios::in | ios::out | ios::binary);
+	if(!inCreditSystem)
+	{
+		cerr << "File could not be opened!" << endl;
+		exit(1);
+	}
+	
+	/*Program Title*/
+	cout<<"----------------"<<endl;
+	cout<<"On-line Shopping"<<endl;
+	cout<<"----------------\n"<<endl;
+	// Print records
+	cout <<"----------------"<<endl;
+	cout<<"Existing Records"<<endl;
+	cout <<"----------------\n"<<endl;
+	client.printRecords("ACCT.dat");
+	// Start the process
+	client.enterClient();
+	
+	return 0;
+}
